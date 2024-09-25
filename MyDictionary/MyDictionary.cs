c15ef2bc876f6567bc0fafdf99360e4a8ef1429b
basak <basak@BASAKSELIN>
2024-09-25 19:27:26 +0300

    KodlamaIO - Ödev

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class MyDictionary<TKeys, TValue>
+    {
+        TKeys[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKeys[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKeys key, TValue value)
+        {
+            TKeys[] tempKey = keys;
+            TValue[] tempValue = values;
+
+            keys = new TKeys[tempKey.Length + 1];
+            values = new TValue[tempValue.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+
+            keys[keys.Length - 1] = key;          
+            values[keys.Length - 1] = value;
+        }
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Numara:{0} - İsim:{1}", keys[i], values[i]);
+            }
+        }
+    }
+}
+
--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionarys = new MyDictionary<int, string>();
+            dictionarys.Add(0, "Başak");
+            dictionarys.Add(1, "Selin");
+            dictionarys.Add(2, "Batuhan");
+            dictionarys.Add(3, "Yaren");
+
+            Console.WriteLine("-----Listele-----");
+            dictionarys.List();
+           
+        }
+    }
+}
